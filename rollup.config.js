cc42feefaa300bc58509bfabd7e21d0f679482ac
mohansella <rkssmohankumar@gmail.com>
2020-04-15 09:11:43 +0530

    suppressed: noisy warnings due to svelte-i18n

--- rollup.config.js
+++ rollup.config.js
@@ -12,7 +12,18 @@ const mode = process.env.NODE_ENV;
 const dev = mode === 'development';
 const legacy = !!process.env.SAPPER_LEGACY_BUILD;
 
-const onwarn = (warning, onwarn) => (warning.code === 'CIRCULAR_DEPENDENCY' && /[/\\]@sapper[/\\]/.test(warning.message)) || onwarn(warning);
+//const onwarn = (warning, onwarn) => (warning.code === 'CIRCULAR_DEPENDENCY' && /[/\\]@sapper[/\\]/.test(warning.message)) || onwarn(warning);
+const onwarn = (warning, onwarn) => {
+	if (
+	  (warning.code === 'CIRCULAR_DEPENDENCY' &&
+		/[/\\]@sapper[/\\]/.test(warning.message)) ||
+	  warning.code === 'THIS_IS_UNDEFINED'
+	) {
+	  return
+	}
+  
+	onwarn(warning)
+  }
 
 export default {
 	client: {
