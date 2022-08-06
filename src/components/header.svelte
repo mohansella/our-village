a50fe23b424a24717b48780ef176de2bfb2ee1fb
Jayasuriyan RP <jayasuriyan.rp@outlook.com>
2022-08-06 08:50:55 +0530

    Youtube Link added and Code Formatting

--- src/components/header.svelte
+++ src/components/header.svelte
@@ -1,274 +1,284 @@
 <script>
-    import { onMount } from 'svelte';
-    import { stores } from '@sapper/app';
-    import { createPopper } from '@popperjs/core';
+  import { onMount } from "svelte";
+  import { stores } from "@sapper/app";
+  import { createPopper } from "@popperjs/core";
 
-    import { enLocale, taLocale, _ } from "@/services/LocaleService.js";
+  import { enLocale, taLocale, _ } from "@/services/LocaleService.js";
 
-    import LogoSvg from "@/components/svgs/logo.svelte";
-    import HeartSvg from "@/components/svgs/heart.svelte";
-    import InstagramSvg from "@/components/svgs/instagram.svelte";
-    import FacebookSvg from "@/components/svgs/facebook.svelte";
-    import DiscordSvg from "@/components/svgs/discord.svelte";
-    import BarsSvg from "@/components/svgs/bars.svelte";
-    
-    
-    export let segment = "";
+  import LogoSvg from "@/components/svgs/logo.svelte";
+  import HeartSvg from "@/components/svgs/heart.svelte";
+  import InstagramSvg from "@/components/svgs/instagram.svelte";
+  import FacebookSvg from "@/components/svgs/facebook.svelte";
+  import DiscordSvg from "@/components/svgs/discord.svelte";
+  import BarsSvg from "@/components/svgs/bars.svelte";
+  import YoutubeSvg from "@/components/svgs/youtube.svelte";
 
-    let otherLangUrl = ".";
-    let otherLocale = taLocale;
+  export let segment = "";
 
-    let heartEle;
-    let heartListEle;
-    let linksEle;
-    let popperInstance;
+  let otherLangUrl = ".";
+  let otherLocale = taLocale;
 
-    const links = [ "culture", "tourism", "events", "gallery", "blogs" ];
-    const { page, preloading, session } = stores();
+  let heartEle;
+  let heartListEle;
+  let linksEle;
+  let popperInstance;
 
-    $: {
-        let match = /^\/(\w+)(.*?)$/.exec($page.path);
-        if(match) {
-            otherLocale = match[1] == enLocale.name ? taLocale : enLocale;
-            otherLangUrl = "/" + otherLocale.name + match[2];
-        }
+  const links = ["culture", "tourism", "events", "gallery", "blogs"];
+  const { page, preloading, session } = stores();
+
+  $: {
+    let match = /^\/(\w+)(.*?)$/.exec($page.path);
+    if (match) {
+      otherLocale = match[1] == enLocale.name ? taLocale : enLocale;
+      otherLangUrl = "/" + otherLocale.name + match[2];
     }
+  }
 
-    function showTT() {
-        heartListEle.style.display = "";
-        popperInstance.update();
+  function showTT() {
+    heartListEle.style.display = "";
+    popperInstance.update();
+  }
+
+  function hideTT() {
+    heartListEle.style.display = "none";
+    popperInstance.update();
+  }
+
+  function initalizePopper() {
+    popperInstance = createPopper(heartEle, heartListEle, {
+      placement: "bottom",
+      modifiers: [
+        {
+          name: "offset",
+          options: {
+            offset: [0, 8],
+          },
+        },
+      ],
+    });
+
+    return destroyPopper;
+  }
+
+  function destroyPopper() {
+    if (popperInstance) {
+      popperInstance.destroy();
     }
+  }
 
-    function hideTT() {
-        heartListEle.style.display = "none";
-        popperInstance.update();
-    }
+  function toggleBars() {
+    linksEle.classList.toggle("show");
+  }
 
-    function initalizePopper() {
-        popperInstance = createPopper(heartEle, heartListEle, {
-            placement: 'bottom',
-            modifiers: [
-                {
-                    name: 'offset',
-                    options: {
-                        offset: [0, 8],
-                    },
-                },
-            ],
-        });
-
-        return destroyPopper;
-    }
-
-    function destroyPopper() {
-        if(popperInstance) {
-            popperInstance.destroy();
-        }
-    }
-
-    function toggleBars() {
-        linksEle.classList.toggle('show');
-    }
-    
-    onMount(initalizePopper);
-
+  onMount(initalizePopper);
 </script>
 
-<svelte:window on:click={hideTT}/>
+<svelte:window on:click={hideTT} />
 
 <nav>
-    
-    <a class="brand" href="{segment}">
-        <LogoSvg/>
-        <span>{$_(`titles.main.disp`)}</span>
-    </a>
-    
-    <ul class="links"  bind:this={linksEle}>
-        {#each links as link}
-            <li>
-                <a href="{segment}/{link}">{$_(`header.${link}`)}</a>
-            </li>
-        {/each}
-        <li>
-            <a href="{otherLangUrl}">{otherLocale.display}</a>
-        </li>
-        <li class="heart" bind:this={heartEle} on:click|stopPropagation={showTT}>
-            <HeartSvg/>
-            
-            <ul class="heart-tt" bind:this={heartListEle} role="tooltip" style="display: none" on:mouseleave={hideTT}>
-                <a href=".">
-                    <InstagramSvg/>
-                    <span>{$_(`header.instagram`)}</span>
-                </a>
-                <a href=".">
-                    <FacebookSvg/>
-                    <span>{$_(`header.facebook`)}</span>
-                </a>
-                <a href=".">
-                    <DiscordSvg/>
-                    <span>{$_(`header.discord`)}</span>
-                </a>
-                <div class="heart-tt-arrow" data-popper-arrow></div>
-            </ul>
-        </li>
-    </ul>
-    
-    <div class="bars" on:click={toggleBars}><BarsSvg/></div>
+  <a class="brand" href={segment}>
+    <LogoSvg />
+    <span>{$_(`titles.main.disp`)}</span>
+  </a>
 
+  <ul class="links" bind:this={linksEle}>
+    {#each links as link}
+      <li>
+        <a href="{segment}/{link}">{$_(`header.${link}`)}</a>
+      </li>
+    {/each}
+    <li>
+      <a href={otherLangUrl}>{otherLocale.display}</a>
+    </li>
+    <li class="heart" bind:this={heartEle} on:click|stopPropagation={showTT}>
+      <HeartSvg />
+
+      <ul
+        class="heart-tt"
+        bind:this={heartListEle}
+        role="tooltip"
+        style="display: none"
+        on:mouseleave={hideTT}
+      >
+        <a href=".">
+          <InstagramSvg />
+          <span>{$_(`header.instagram`)}</span>
+        </a>
+        <a href=".">
+          <FacebookSvg />
+          <span>{$_(`header.facebook`)}</span>
+        </a>
+        <a href=".">
+          <DiscordSvg />
+          <span>{$_(`header.discord`)}</span>
+        </a>
+        <a
+          href="https://www.youtube.com/watch?v=EOwjX1c37qw&ab_channel=ViewOverviewVlogs"
+          target="_blank"
+        >
+          <YoutubeSvg />
+          <span>{$_(`header.youtube`)}</span>
+        </a>
+        <div class="heart-tt-arrow" data-popper-arrow />
+      </ul>
+    </li>
+  </ul>
+
+  <div class="bars" on:click={toggleBars}><BarsSvg /></div>
 </nav>
 
 <style>
+  /* navigation main */
+  nav {
+    min-height: 56px;
+    padding: 8px 16px;
+    display: flex;
+    justify-content: space-around;
 
-    /* navigation main */
+    width: 95%;
+    margin: auto;
+    max-width: 1600px;
+  }
+
+  /* brand */
+  .brand {
+    flex: 4;
+    display: flex;
+    align-items: center;
+    font-size: 20px;
+    max-width: 160px;
+    margin-right: auto;
+    min-height: 40px;
+  }
+
+  .brand > :global(svg) {
+    height: 25px;
+    margin: 0px 5px;
+  }
+
+  /* links */
+  .links {
+    display: flex;
+    align-items: center;
+    padding: 0px;
+    margin: 0px;
+    list-style-type: none;
+  }
+
+  .links > li {
+    padding: 0px 10px;
+    font-size: 16px;
+  }
+
+  .links > li > a:hover {
+    color: black;
+    border-bottom: 2px solid black;
+  }
+
+  /* heart */
+  .heart > :global(svg) {
+    width: 16px;
+    cursor: pointer;
+    opacity: 0.7;
+  }
+  .heart:hover > :global(svg:hover) {
+    opacity: 1;
+  }
+  .heart-tt {
+    display: flex;
+    align-items: center;
+    padding: 0px;
+    margin: 0px;
+    list-style-type: none;
+    position: absolute;
+    flex-direction: column;
+    align-items: baseline;
+    background: white;
+    box-shadow: 4px 2px 12px #888888;
+    z-index: 100;
+  }
+
+  .heart-tt-arrow,
+  .heart-tt-arrow::before {
+    position: absolute;
+    width: 8px;
+    height: 8px;
+    z-index: -1;
+  }
+
+  .heart-tt-arrow::before {
+    content: "";
+    transform: rotate(45deg);
+    background: white;
+  }
+
+  :global(.heart-tt[data-popper-placement^="top"] > .heart-tt-arrow) {
+    bottom: -4px;
+  }
+
+  :global(.heart-tt[data-popper-placement^="bottom"] > .heart-tt-arrow) {
+    top: -4px;
+  }
+
+  :global(.heart-tt[data-popper-placement^="left"] > .heart-tt-arrow) {
+    right: -4px;
+  }
+
+  :global(.heart-tt[data-popper-placement^="right"] > .heart-tt-arrow) {
+    left: -4px;
+  }
+
+  .heart-tt > a {
+    display: flex;
+    margin: 6px 15px;
+  }
+  .heart-tt > a > :global(svg) {
+    height: 16px;
+    margin-top: 2px;
+    margin-right: 4px;
+  }
+
+  .bars {
+    height: 20px;
+    width: 20px;
+    border: none;
+    position: absolute;
+    top: 18px;
+    right: 22px;
+    display: none;
+    cursor: pointer;
+  }
+
+  .bars:hover {
+    opacity: 0.9;
+  }
+
+  @media (max-width: 800px) {
+    .bars {
+      display: inline-block;
+    }
+
     nav {
-        min-height: 56px;
-        padding: 8px 16px;
-        display: flex;
-        justify-content: space-around;
-
-        width: 95%;
-        margin: auto;
-        max-width: 1600px;
+      flex-direction: column;
+      width: 100%;
     }
 
-    /* brand */
-    .brand {
-        flex: 4;
-        display: flex;
-        align-items: center;
-        font-size: 20px;
-        max-width: 160px;
-        margin-right: auto;
-        min-height: 40px;
-    }
-
-    .brand > :global(svg) {
-        height: 25px;
-        margin: 0px 5px;
-    }
-
-    /* links */
     .links {
-        display: flex;
-        align-items: center;
-        padding: 0px;
-        margin: 0px;
-        list-style-type: none;
+      display: none;
+      flex-direction: column;
+      align-items: flex-start;
+      margin: 15px;
+    }
+
+    :global(.links.show) {
+      display: flex !important;
     }
 
     .links > li {
-        padding: 0px 10px;
-        font-size: 16px;
+      margin: 10px;
+      width: 95%;
+      border-bottom: 1px solid #b5b5b5;
+      padding-bottom: 8px;
     }
-
-    .links > li > a:hover {
-        color: black;
-        border-bottom: 2px solid black;
-    }
-
-    /* heart */
-    .heart > :global(svg) {
-        width: 16px;
-        cursor: pointer;
-        opacity: 0.7;
-    }
-    .heart:hover > :global(svg:hover) {
-        opacity: 1;
-    }
-    .heart-tt {
-        display: flex;
-        align-items: center;
-        padding: 0px;
-        margin: 0px;
-        list-style-type: none;
-        position: absolute;
-        flex-direction: column;
-        align-items: baseline;
-        background: white;
-        box-shadow: 4px 2px 12px #888888;
-        z-index: 100;
-    }
-
-    .heart-tt-arrow, .heart-tt-arrow::before {
-        position: absolute;
-        width: 8px;
-        height: 8px;
-        z-index: -1;
-    }
-
-    .heart-tt-arrow::before {
-        content: '';
-        transform: rotate(45deg);
-        background: white;
-    }
-
-    :global(.heart-tt[data-popper-placement^='top'] > .heart-tt-arrow) {
-        bottom: -4px;
-    }
-
-    :global(.heart-tt[data-popper-placement^='bottom'] > .heart-tt-arrow) {
-        top: -4px;
-    }
-
-    :global(.heart-tt[data-popper-placement^='left'] > .heart-tt-arrow) {
-        right: -4px;
-    }
-
-    :global(.heart-tt[data-popper-placement^='right'] > .heart-tt-arrow) {
-        left: -4px;
-    }
-
-    .heart-tt > a {
-        display: flex;
-        margin: 6px 15px;
-    }
-    .heart-tt > a > :global(svg) {
-        height: 16px;
-        margin-top: 2px;
-        margin-right: 4px;
-    }
-
-    .bars {
-        height: 20px;
-        width: 20px;
-        border: none;
-        position: absolute;
-        top: 18px;
-        right: 22px;
-        display: none;
-        cursor: pointer;
-    }
-
-    .bars:hover {
-        opacity: 0.9;
-    }
-
-    @media(max-width: 800px) {
-        .bars {
-            display: inline-block;
-        }
-        
-        nav {
-            flex-direction: column;
-            width: 100%;
-        }
-
-        .links {
-            display: none;
-            flex-direction: column;
-            align-items: flex-start;
-            margin: 15px;
-        }
-
-        :global(.links.show) {
-            display: flex !important;
-        }
-
-        .links > li {
-            margin: 10px;
-            width: 95%;
-            border-bottom: 1px solid #b5b5b5;
-            padding-bottom: 8px;
-        }
-    }
+  }
 </style>
--- src/routes/[lang]/index.svelte
+++ src/routes/[lang]/index.svelte
@@ -1,13 +1,25 @@
 <script>
-    import Carousel from '@beyonk/svelte-carousel'
-    import { ChevronLeftIcon, ChevronRightIcon } from 'svelte-feather-icons'
-    import { _ } from "@/services/LocaleService.js";
+  import Carousel from "@beyonk/svelte-carousel";
+  import { ChevronLeftIcon, ChevronRightIcon } from "svelte-feather-icons";
+  import { _ } from "@/services/LocaleService.js";
 
-    const images = [
-        { src: 'IMG_20200117_154938.jpg', alt: 'Poolampatti landscape 1', heading: 'carousel.nature'},
-        { src: 'IMG_20181223_072757-01.jpg', alt: 'Poolampatti landscape 2', heading: 'carousel.heaven'},
-        { src: 'IMG_20190907_140811.jpg', alt: 'Poolampatti landscape 3', heading: 'carousel.beauty'}
-    ];
+  const images = [
+    {
+      src: "IMG_20200117_154938.jpg",
+      alt: "Poolampatti landscape 1",
+      heading: "carousel.nature",
+    },
+    {
+      src: "IMG_20181223_072757-01.jpg",
+      alt: "Poolampatti landscape 2",
+      heading: "carousel.heaven",
+    },
+    {
+      src: "IMG_20190907_140811.jpg",
+      alt: "Poolampatti landscape 3",
+      heading: "carousel.beauty",
+    },
+  ];
 </script>
 
 <Carousel perPage={1} autoplay={15000} loop={true}>
@@ -16,7 +28,11 @@
   </span>
   {#each images as image}
     <div class="container">
-      <img class="slide-content" src={`images/landscape/${image.src}`} alt={image.alt}>
+      <img
+        class="slide-content"
+        src={`images/landscape/${image.src}`}
+        alt={image.alt}
+      />
       <div class="centered-div">
         <header>
           <p>{$_("carousel.gods_own_village")}</p>
@@ -29,78 +45,78 @@
     <ChevronRightIcon />
   </span>
 </Carousel>
- 
+
 <style>
+  .slide-content {
+    width: 100%;
+    height: 800px;
+    object-fit: cover;
+  }
+  .control :global(svg) {
+    width: 100%;
+    height: 100%;
+    color: #fff;
+    border: 2px solid #fff;
+    border-radius: 32px;
+  }
+  .container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: black;
+  }
+  .container > img {
+    opacity: 0.9;
+  }
+  .centered-div {
+    position: absolute;
+    margin: 10px;
+    padding: 5px;
+    width: 100%;
+    text-align: center;
+  }
+  .centered-div > header > p {
+    color: rgba(255, 255, 255, 0.8);
+    text-transform: uppercase;
+    font-size: 1rem;
+    font-weight: 300;
+    margin: 0;
+    padding-bottom: 1.75rem;
+    letter-spacing: 0.25rem;
+    text-shadow: 0 0 25px #000;
+  }
+  .centered-div > header > p::after {
+    content: "";
+    position: absolute;
+    margin: auto;
+    right: 0;
+    bottom: 130px;
+    left: 0;
+    width: 170px;
+    height: 1px;
+    background-color: rgba(255, 255, 255, 0.65);
+  }
+  .centered-div > header > h2 {
+    font-size: 7rem;
+    margin-bottom: 0;
+    color: #fff;
+    font-weight: 300;
+  }
+
+  @media (max-width: 1200px) {
     .slide-content {
-        width: 100%;
-        height: 800px;
-        object-fit: cover;
+      height: 700px;
     }
-    .control :global(svg) {
-        width: 100%;
-        height: 100%;
-        color: #fff;
-        border: 2px solid #fff;
-        border-radius: 32px;
-    }
-    .container {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        background: black;
-    }
-    .container > img {
-        opacity: 0.90;
-    }
-    .centered-div {
-        position: absolute;
-        margin: 10px;
-        padding:5px;
-        width: 100%;
-        text-align: center;
-    }
-    .centered-div > header > p {
-        color: rgba(255, 255, 255, 0.8);
-        text-transform: uppercase;
-        font-size: 1rem;
-        font-weight: 300;
-        margin: 0;
-        padding-bottom: 1.75rem;
-        letter-spacing: .25rem;
-        text-shadow: 0 0 25px #000;
-    }
-    .centered-div > header > p::after {
-        content: '';
-        position: absolute;
-        margin: auto;
-        right: 0;
-        bottom: 130px;
-        left: 0;
-        width: 170px;
-        height: 1px;
-        background-color: rgba(255, 255, 255, 0.65);
+  }
+  @media (max-width: 800px) {
+    .slide-content {
+      height: 470px;
     }
     .centered-div > header > h2 {
-      font-size: 7rem;
-      margin-bottom: 0;
-      color: #FFF;
-      font-weight: 300;
+      font-size: 4rem;
     }
-
-    @media(max-width: 1200px) {
-        .slide-content {
-            height: 700px;
-        }
+    .centered-div > header > p::after {
+      bottom: 85px;
     }
-    @media(max-width: 800px) {
-        .slide-content {
-            height: 470px;
-        }
-        .centered-div > header > h2 {
-          font-size: 4rem;
-        }
-        .centered-div > header > p::after {
-          bottom: 85px;
-        }
-    }
+  }
 </style>
